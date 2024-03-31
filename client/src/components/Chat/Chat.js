1910be45659f76a453d16da8953341d0c90a50e9
mwenda1 <tariromwe@gmail.com>
2024-03-31 00:22:07 -0400

    Fix: Server not sending messages

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -56,6 +56,7 @@ const Chat = () => {
   <div className="outerContainer">
     <div className="container">
       <InfoBar room={room} /> 
+      <Messages messages={messages} name={name} />
       <Input message={message} setMessage={setMessage} sendMessage={sendMessage}/>
     </div>
   </div>
--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
 
         if (error) return callback(error);
 
